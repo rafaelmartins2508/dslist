a82e5c17af4e4b30dbbc93bad27f7c89ea2b1309
rafaelmartins2508 <rafaelmartins2508@gmail.com>
2023-05-10 17:42:08 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -19,25 +19,29 @@ public class Game {
 	@Column(name = "game_year")
 	private Integer year;
 	private String genre;
-	private String plataform;
+	private String platforms;
+	private Double score;
 	private String imgUrl;
-	private String shortDescriptin;
-	private String longDescriptin;
+	@Column(columnDefinition = "TEXT")
+	private String shortDescription;
+	@Column(columnDefinition = "TEXT")
+	private String longDescription;
 	
 	public Game() {
 		
 	}
 
-	public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
-			String shortDescriptin, String longDescriptin) {
+	public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
+			String shortDescription, String longDescription) {
 		this.id = id;
 		this.title = title;
 		this.year = year;
 		this.genre = genre;
-		this.plataform = plataform;
+		this.platforms = platforms;
+		this.score = score;
 		this.imgUrl = imgUrl;
-		this.shortDescriptin = shortDescriptin;
-		this.longDescriptin = longDescriptin;
+		this.shortDescription = shortDescription;
+		this.longDescription = longDescription;
 	}
 
 	public Long getId() {
@@ -72,12 +76,20 @@ public class Game {
 		this.genre = genre;
 	}
 
-	public String getPlataform() {
-		return plataform;
+	public String getPlatforms() {
+		return platforms;
 	}
 
-	public void setPlataform(String plataform) {
-		this.plataform = plataform;
+	public void setPlatforms(String platforms) {
+		this.platforms = platforms;
+	}
+
+	public Double getScore() {
+		return score;
+	}
+
+	public void setScore(Double score) {
+		this.score = score;
 	}
 
 	public String getImgUrl() {
@@ -88,20 +100,20 @@ public class Game {
 		this.imgUrl = imgUrl;
 	}
 
-	public String getShortDescriptin() {
-		return shortDescriptin;
+	public String getShortDescription() {
+		return shortDescription;
 	}
 
-	public void setShortDescriptin(String shortDescriptin) {
-		this.shortDescriptin = shortDescriptin;
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
 	}
 
-	public String getLongDescriptin() {
-		return longDescriptin;
+	public String getLongDescription() {
+		return longDescription;
 	}
 
-	public void setLongDescriptin(String longDescriptin) {
-		this.longDescriptin = longDescriptin;
+	public void setLongDescription(String longDescription) {
+		this.longDescription = longDescription;
 	}
 
 	@Override
